20f6070b6eaf6c7e2f93f48af815342891290a1d
coraljiao <mrjiaohs@163.com>
2018-12-13 12:47:09 +0800

    Springboot注入方式2

--- src/main/java/com/xjtu/config/JdbcConfig.java
+++ src/main/java/com/xjtu/config/JdbcConfig.java
@@ -1,16 +1,20 @@
 package com.xjtu.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
 import javax.sql.DataSource;
 
 @Configuration
-@EnableConfigurationProperties(JdbcProperties.class)
+//@EnableConfigurationProperties(JdbcProperties.class)
 public class JdbcConfig {
 
-//    @Value("${jdbc.url}")
+    //    @Value("${jdbc.url}")
 //    String url;
 //    @Value("${jdbc.driverClassName}")
 //    String driverClassName;
@@ -20,14 +24,27 @@ public class JdbcConfig {
 //    String password;
 //    @Autowired
 //    private JdbcProperties jdbc;
-    @Bean
-    public DataSource dataSource(JdbcProperties jdbc){
+//    @Autowired
+//    JdbcProperties jdbc;
+//
+//    public JdbcConfig(JdbcProperties prop) {
+//        this.jdbc = prop;
+//    }
 
-        DruidDataSource dataSource = new DruidDataSource();
-        dataSource.setUrl(jdbc.getUrl());
-        dataSource.setDriverClassName(jdbc.getDriverClassName());
-        dataSource.setUsername(jdbc.getUsername());
-        dataSource.setPassword(jdbc.getPassword());
-        return dataSource;
+//    @Bean
+//    public DataSource dataSource(JdbcProperties jdbc) {
+//
+//        DruidDataSource dataSource = new DruidDataSource();
+//        dataSource.setUrl(jdbc.getUrl());
+//        dataSource.setDriverClassName(jdbc.getDriverClassName());
+//        dataSource.setUsername(jdbc.getUsername());
+//        dataSource.setPassword(jdbc.getPassword());
+//        return dataSource;
+//    }
+    @Bean
+    @ConfigurationProperties(prefix = "jdbc")
+    public DataSource dataSource() {
+
+        return new DruidDataSource();
     }
 }
--- src/main/java/com/xjtu/config/JdbcProperties.java
+++ src/main/java/com/xjtu/config/JdbcProperties.java
@@ -3,7 +3,7 @@ package com.xjtu.config;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties(prefix = "jdbc")
+//@ConfigurationProperties(prefix = "jdbc")
 @Data
 public class JdbcProperties {
     String url;
